2bd72cf26748ffcd6d3581f757722bb7a13a8873
chrissyalbert <chrissy_albert@yahoo.com>
2020-09-08 13:05:05 -0700

    initial commit: add React, React-Bootstrap, add API keys

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
+
 function App() {
+  const WEATHER_ID = process.env.REACT_APP_WEATHER_ID;
+  const CONSUMER_KEY= process.env.REACT_APP_WEATHER_CONSUMER_KEY;
+  const CLIENT_SECRET= process.env.REACT_APP_WEATHER_CLIENT_SECRET;
+
+  const [city, setCity ] = useState("");
+  
+  
+
+  var OAuth = require('oauth');
+  var header = {
+    "X-Yahoo-App-Id": WEATHER_ID, 
+  };
+  var request = new OAuth.OAuth(
+    null,
+    null,
+    CONSUMER_KEY,
+    CLIENT_SECRET,
+    '1.0',
+    null,
+    'HMAC-SHA1',
+    null,
+    header
+);
+request.get(
+    'https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json',
+    null,
+    null,
+    function (err, data, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data)
+        }
+    }
+);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+    <InputGroup.Text id="basic-addon1">Get the weather for your favorite city.</InputGroup.Text>
+    <br></br>
+      <InputGroup.Text id="basic-addon1">What city?</InputGroup.Text>
+    </InputGroup.Prepend>
+    <Form.Control
+      placeholder="What city?"
+      aria-label="What city?"
+      aria-describedby="basic-addon1"
+      
+    />
+  </InputGroup>
     </div>
   );
 }
 
 export default App;
+
+/*
+function stringMaker() {
+  let city = "phoenix";
+  let queryUrl = "https://weather-ydn-yql.media.yahoo.com/forecastrss?";
+  queryUrl += city;
+  return queryUrl;
+}
+
+const handleChange = (event) => {
+  setCity(event.target.value);
+}
+onChange={handleChange}
+*/
